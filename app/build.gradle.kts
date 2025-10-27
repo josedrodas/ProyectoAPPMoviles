ad5224f5fb6b0bbcfbc620d059489594a4088310
JoseRodas <j.rodas@duocuc.cl>
2025-10-27 10:11:12 -0300

    V 1.0 TARIAMOS CHAVALES

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,6 @@ android {
     }
 
     composeOptions {
-        // Se maneja vía BOM, pero este bloque es harmless si lo dejas vacío
-        // kotlinCompilerExtensionVersion = "x.y.z"
     }
 
     compileOptions {
@@ -41,25 +39,23 @@ android {
 }
 
 dependencies {
-    // Compose BOM: centraliza versiones de Compose
+
     implementation(platform("androidx.compose:compose-bom:2024.10.00"))
 
-    // Core
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
 
-    // Compose UI + Material3 + Tooling preview
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
-    // Foundation (NECESARIO para Image, Row/Column, etc.)
     implementation("androidx.compose.foundation:foundation")
 
-    // Navegación Compose
+
     implementation("androidx.navigation:navigation-compose:2.8.0")
 
-    // ViewModel para Compose
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
 
     // Debug tooling
